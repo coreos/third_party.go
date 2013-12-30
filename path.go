3045ae865aa71e47d40a79b6d51d12feb1539b97
Brandon Philips <brandon@ifup.co>
2013-12-29 17:06:26 -0800

    fix(path): remove build.go references

--- path.go
+++ path.go
@@ -16,10 +16,10 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// This program builds a project and is a copy of build.go. See
-// github.com/philips/build.go
+// This program builds a project and is a copy of path.go. See
+// github.com/philips/path.go
 //
-// $ go run build.go
+// $ go run path.go
 //
 // See the README file for more details.
 package main
